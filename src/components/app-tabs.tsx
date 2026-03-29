820653d90fe95b8305acd5974c0eca80869993e4
antmoute <lejugeti@gmail.com>
2026-03-29 11:38:41 +0200

    recreate index.tsx so that app starts properly

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -5,29 +5,29 @@ import { useColorScheme } from 'react-native';
 import { Colors } from '@/constants/theme';
 
 export default function AppTabs() {
-  const scheme = useColorScheme();
-  const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
+    const scheme = useColorScheme();
+    const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
 
-  return (
-    <NativeTabs
-      backgroundColor={colors.background}
-      indicatorColor={colors.backgroundElement}
-      labelStyle={{ selected: { color: colors.text } }}>
-          <NativeTabs.Trigger name="play-page">
-              <NativeTabs.Trigger.Label>Play</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/home.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
+    return (
+        <NativeTabs
+            backgroundColor={colors.background}
+            indicatorColor={colors.backgroundElement}
+            labelStyle={{ selected: { color: colors.text } }}>
+            <NativeTabs.Trigger name="play-page">
+                <NativeTabs.Trigger.Label>Play</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    src={require('@/assets/images/tabIcons/home.png')}
+                    renderingMode="template"
+                />
+            </NativeTabs.Trigger>
 
-      <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
-    </NativeTabs>
-  );
+            <NativeTabs.Trigger name="explore">
+                <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    src={require('@/assets/images/tabIcons/explore.png')}
+                    renderingMode="template"
+                />
+            </NativeTabs.Trigger>
+        </NativeTabs>
+    );
 }

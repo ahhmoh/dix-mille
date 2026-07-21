c7a53846f1eda58c54f2e8999d36ab41b4d4e0d7
antmoute <lejugeti@gmail.com>
2026-07-21 17:46:25 +0200

    prevent zooming

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -16,7 +16,7 @@ export default function Root({ children }: PropsWithChildren) {
         />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          content='width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no'
         />
 
         {/* 
